1a083184c16eb63d6e112546a7e95ee9ad67c849
Maxssobolev <sobolev-ms@inbox.ru>
2024-09-19 09:41:54 +0300

    update setup

--- src/shared/utils/converseType.ts
+++ src/shared/utils/converseType.ts
@@ -1,8 +1,8 @@
 import { TimetableTypes } from "../../models/timetable.model";
 
 export const converseType = (value: string) => {
-  switch (value.trim()) {
-    case 'Лек, Пр':
+  switch (value.trim().replaceAll(' ', '')) {
+    case 'Лек,Пр':
       return TimetableTypes.both;
     case 'Лаб':
       return TimetableTypes.lab;
